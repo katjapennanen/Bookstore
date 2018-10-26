9ba499214ae9b0796efdd8620d1daf4ad9040540
Katja Pennanen <katja.pennanen@myy.haaga-helia.fi>
2018-10-26 21:17:12 +0300

    Spring Security added

--- src/main/java/fi/haagahelia/sp/bookstore/BookstoreApplication.java
+++ src/main/java/fi/haagahelia/sp/bookstore/BookstoreApplication.java
@@ -7,10 +7,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+
 import fi.haagahelia.sp.bookstore.domain.Book;
 import fi.haagahelia.sp.bookstore.domain.BookRepository;
 import fi.haagahelia.sp.bookstore.domain.Category;
 import fi.haagahelia.sp.bookstore.domain.CategoryRepository;
+import fi.haagahelia.sp.bookstore.domain.User;
+import fi.haagahelia.sp.bookstore.domain.UserRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -21,9 +24,9 @@ public class BookstoreApplication {
 	}
 
 	@Bean
-	public CommandLineRunner bookDemo(BookRepository brepository, CategoryRepository crepository) {
+	public CommandLineRunner bookDemo(BookRepository brepository, CategoryRepository crepository, UserRepository urepository) {
 		return (args) -> {
-			log.info("save a couple of books");
+			log.info("save a couple of books and create categories");
 
 			crepository.save(new Category("Horror"));
 			crepository.save(new Category("Fantasy"));
@@ -31,16 +34,17 @@ public class BookstoreApplication {
 			crepository.save(new Category("Fiction"));
 			crepository.save(new Category("Graphic Novel"));
 
-			brepository.save(new Book("It", "Stephen King", "1234567890123", 1986, 20.99,
-					crepository.findByName("Horror").get(0)));
-			brepository.save(new Book("Return of the King", "J.R.R. Tolkien", "1234567890123", 1955, 20.12,
-					crepository.findByName("Fantasy").get(0)));
-			brepository.save(new Book("The Hitchhiker's Guide to the Galaxy", "Douglas Adams", "1234567890123", 1980,
-					20.99, crepository.findByName("Science Fiction").get(0)));
-			brepository.save(new Book("And the Ass Saw the Angel", "Nick Cave", "1234567890123", 1989, 20.99,
-					crepository.findByName("Fiction").get(0)));
-			brepository.save(new Book("Maus", "Art Spiegelman", "1234567890123", 1991, 20.99,
-					crepository.findByName("Graphic Novel").get(0)));
+			brepository.save(new Book("It", "Stephen King", "1234567890123", 1986, 20.99, crepository.findByName("Horror").get(0)));
+			brepository.save(new Book("Return of the King", "J.R.R. Tolkien", "1234567890123", 1955, 20.12, crepository.findByName("Fantasy").get(0)));
+			brepository.save(new Book("The Hitchhiker's Guide to the Galaxy", "Douglas Adams", "1234567890123", 1980, 20.99, crepository.findByName("Science Fiction").get(0)));
+			brepository.save(new Book("And the Ass Saw the Angel", "Nick Cave", "1234567890123", 1989, 20.99, crepository.findByName("Fiction").get(0)));
+			brepository.save(new Book("Maus", "Art Spiegelman", "1234567890123", 1991, 20.99, crepository.findByName("Graphic Novel").get(0)));
+			
+			// Create users: admin/admin user/user
+			User user1 = new User("user", "$2a$04$19ycEAQTqV66Cg4bZt9oR.xTEzfEaEXLrr/dSsOBVIjNr/ep3wtEm", "USER");
+			User user2 = new User("admin", "$2a$04$txKbHel.N18yPJDEufmO..KFpUHaCa9AY7Am.fQdTou8uE8qZgOU6", "ADMIN");
+			urepository.save(user1);
+			urepository.save(user2);
 
 			log.info("fetch all books");
 			for (Book book : brepository.findAll()) {
--- src/main/java/fi/haagahelia/sp/bookstore/web/BookController.java
+++ src/main/java/fi/haagahelia/sp/bookstore/web/BookController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,9 +24,15 @@ public class BookController {
 
 	@Autowired
 	private CategoryRepository crepository;
+	
+	// Login page listener
+    @RequestMapping(value="/login")
+    public String login() {	
+        return "login";
+    }
 
 	// Show all books
-	@RequestMapping(value = "/booklist")
+	@RequestMapping(value ="/booklist")
 	public String bookList(Model model) {
 		model.addAttribute("books", repository.findAll());
 		return "booklist";
@@ -44,6 +51,7 @@ public class BookController {
 	}
 	
 	// Add new book
+	@PreAuthorize("hasAuthority('ADMIN')")
 	@RequestMapping(value = "/add")
 	public String addBook(Model model) {
 		model.addAttribute("book", new Book());
@@ -59,6 +67,7 @@ public class BookController {
 	}
 
 	// Delete book
+	@PreAuthorize("hasAuthority('ADMIN')")
 	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
 	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
 		repository.deleteById(bookId);
